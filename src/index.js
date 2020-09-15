7746a63c6da33370cecfb35bdb015f8fc537c3a4
apolopena <penavilla@gmail.com>
2020-09-15 16:27:22 -0700

    conditionally rendered submenu working version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+
+import headerData from "./headerData.json";
 
 import "./styles.css";
 
 function App() {
-  return <h1>Hello React</h1>;
+  const [menuIndex, setMenuIndex] = useState(-1);
+  return (
+    <div id="navbarHolder">
+      {headerData.navbar.map((item, index) => {
+        return (
+          <div
+            key={index}
+            onMouseOver={
+              item.hasOwnProperty("submenu")
+                ? () => setMenuIndex(index)
+                : () => setMenuIndex(-1)
+            }
+          >
+            {item.text}
+          </div>
+        );
+      })}
+      {menuIndex > -1 && (
+        <div>
+          {headerData.navbar[menuIndex].submenu.map((item) => {
+            return <li>{item.submenuText}</li>;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 const rootElement = document.getElementById("root");
